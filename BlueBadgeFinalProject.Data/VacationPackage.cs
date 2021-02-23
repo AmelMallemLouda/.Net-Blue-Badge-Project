6512ddd052d88379ced145095ace71cbb72b1e3f
CACorpusV526 <73607854+CACorpusV526@users.noreply.github.com>
2021-02-23 17:04:00 -0500

    VacationPackageItems

--- BlueBadgeFinalProject.Data/VacationPackage.cs
+++ BlueBadgeFinalProject.Data/VacationPackage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,22 @@ namespace BlueBadgeFinalProject.Data
         [Key]
         public int VacId { get; set; }
 
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string VacationPackageName { get; set; }
+        [Required]
         public string Flight { get; set; }
+        [Required]
         public string Food { get; set; }
+        [Required]
         public string Transportation { get; set; }
-        public double Price { get; set; }
+        [Required]
+        public string Price { get; set; }
 
+        /*[ForeignKey(nameof(Hotels))]
+        public int HotelId { get; set; }
+        public virtual Hotel Hotels { get; set; }
+        */
     }
 }
--- BlueBadgeFinalProject.Services/VacationPackageService.cs
+++ BlueBadgeFinalProject.Services/VacationPackageService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BlueBadgeFinalProject.Data;
+using BlueBadgeFinalProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,106 @@ using System.Threading.Tasks;
 
 namespace BlueBadgeFinalProject.Services
 {
-   public class VacationPackageService
+    public class VacationPackageService
     {
+        private readonly Guid _userId;
+
+        public VacationPackageService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateVacPac(VacationPackageCreate model)
+        {
+            var entity =
+                new VacationPackage()
+                {
+                    OwnerId = _userId,
+                    VacationPackageName = model.VacPacName,
+                    Transportation = model.Transportation,
+                    Price = model.Price
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.VacationPackage.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<VacationPackageListItem> GetVacPacs()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .VacationPackage
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(e =>
+                    new VacationPackageListItem
+                    {
+                        VacId = e.VacId,
+                        VacPacName = e.VacationPackageName,
+                    }
+                    );
+
+                return query.ToArray();
+            }
+        }
+
+        public VacationPackageDetail GetVacPacsById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .VacationPackage
+                    .Single(e => e.VacId == id && e.OwnerId == _userId);
+                return
+                    new VacationPackageDetail
+                    {
+                        VacId = entity.VacId,
+                        Flight = entity.Flight,
+                        Food = entity.Food,
+                        Price = entity.Price.ToString(),
+                        Transportation = entity.Transportation,
+                        VacationPackageName = entity.VacationPackageName
+                    };
+            }
+        }
+
+        public bool UpdateVacPac(VacationEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .VacationPackage
+                    .Single(e => e.VacId == model.VacId && e.OwnerId == _userId);
+
+                entity.VacationPackageName = model.VacationPackageName;
+                entity.Price = model.Price;
+                entity.Transportation = model.Transportation;
+                entity.Flight = model.Flight;
+                entity.Food = model.Food;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteVacPac(int vacId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .VacationPackage
+                    .Single(e => e.VacId == vacId && e.OwnerId == _userId);
+
+                ctx.VacationPackage.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
