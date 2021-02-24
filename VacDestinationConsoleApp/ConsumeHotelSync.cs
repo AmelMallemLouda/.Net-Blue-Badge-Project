5f2039018e4ef28fa11d0f36a5a984e881c8bac7
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-02-23 20:37:27 -0500

    VS LastVirsion Updated

---
+++ VacDestinationConsoleApp/ConsumeHotelSync.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacDestinationConsoleApp
+{
+   public class ConsumeHotelSync
+    {
+        public void GetAllHotelsData()
+        {
+            using( var client=new WebClient())// WebClient
+            {
+                client.Headers.Add("Content-Type:application/json");
+                client.Headers.Add("Accept:application/json");
+                var result = client.DownloadString("https://localhost:44396/api/Hotel");//URI
+                Console.WriteLine(Environment.NewLine + result);
+            }
+        }
+    }
+}
