5b1bd59226e98e607f8b8f8b8658dd8277808172
nbelghozlane <nbelghoz@gmail.com>
2021-02-25 18:22:08 -0500

    Pushing before pulling from Develop

---
+++ BlueBadgeFinalProject.Data/Review.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadgeFinalProject.Data
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+
+        [Required]
+        public DateTimeOffset DateOfReview { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        /*[ForeignKey (nameof(Hotel))]
+        public int HotelId { get; set; }
+        public virtual Hotel Hotel { get; set; }*/
+    }
+}
