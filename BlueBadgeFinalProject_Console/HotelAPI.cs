090403f7b4df28944f8319987bf74f6f59f9fb0e
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-03-02 17:18:43 -0500

    Started ConsoleApp

--- BlueBadgeFinalProject_Console/HotelAPI.cs
+++ BlueBadgeFinalProject_Console/HotelAPI.cs
@@ -10,7 +10,7 @@ namespace BlueBadgeFinalProject_Console
 {
     public class HotelAPI
     {
-        private string AuthorizationKey ="";
+        private string AuthorizationKey = "BBd5b1VSaHiZDZ4Hhb31NdG5xzOQdvbPeZQn0ac_cGgezIlnKjDkcVvjfWSV8BS46huq218OjP9OUgumywtVMgV27tSXVeYaIE7yyWWKdcaTyQ-xuzX29w3-b353jQ1Icz34BSgnoMu76iEFe6xKulSwwHmziFx-AcNXUHwqIe9FeR6uuWrX7H0ihuImgCEk9QQ-GHWycUxHe_n0ocKbtHjDGf4i4J9ENuoeXwllOOX5IG0VmX8WH81XanzACAV8kaGtVTodixlIHOwskDZfxcqtwrfCuWGZaCiKgQ64r-z3MbUdZ6qebMNAFgSh6dnVB53ZeBLJBM9DEEuNFD-AjrIPwTvowSWlb__YX9D2H6zU0dHp4MctcOmQorJKCk_p2gUD3hrxqMw68pOEGo3Pq5tgADnnz5gFiZctQI5q_9ZUsSAd9Tqm9HdKvog1AJzkeWrNS75XkVAtFhrwBc45qLP3QgYti7cSc9L1XVyktdu58kfMP6gfpdp1iKtw_0LV";
 
         public HotelAPI(string authorizationKey)
         {
