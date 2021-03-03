b8b334c48c0e1ebd150541fc468e769f2eaa57cc
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-03-03 18:32:52 -0500

    pulling from Develop2

--- BlueBadgeFinalProject.Models/HotelModels/HotelDetails.cs
+++ BlueBadgeFinalProject.Models/HotelModels/HotelDetails.cs
@@ -1,11 +1,5 @@
 ﻿using BlueBadgeFinalProject.Models.CustomerFolder;
-using BlueBadgeFinalProject.Models.ReviewModels;
-using BlueBadgeFinalProject.Models.TransactionModels;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlueBadgeFinalProject.Models.HotelModels
 {
@@ -17,7 +11,7 @@ namespace BlueBadgeFinalProject.Models.HotelModels
         public bool HasFreeParking { get; set; }
         public virtual List<CustomerList> Customers { get; set; } = new List<CustomerList>();
         public virtual List<VacationPackageListItem> VacationPackages { get; set; } = new List<VacationPackageListItem>();
-        public List<ReviewListItem> Reviews { get; set; } = new List<ReviewListItem>();
+       // public List<ReviewListItem> Reviews { get; set; } = new List<ReviewListItem>();
 
     }
 }
